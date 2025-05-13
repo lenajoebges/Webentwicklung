4aefc415e6adba0e8cb16233bbe8f5cabd0b4335
lenajoebges <ljo46499@stud.hs-furtwangen.de>
2025-05-13 23:40:29 +0200

    index update

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const moodButtons = document.querySelectorAll(".mood-btn");
-
+  
     moodButtons.forEach(button => {
-        button.addEventListener("click", function () {
-            const url = new URL(button.href);
-            const selectedMood = url.searchParams.get("mood");
-
-            localStorage.setItem("lastMood", selectedMood);
-
-            button.classList.add("clicked");
-            setTimeout(() => button.classList.remove("clicked"), 200);
+      button.addEventListener("click", function () {
+        const url = new URL(button.href);
+        const selectedMood = url.searchParams.get("mood");
         });
     });
 });
